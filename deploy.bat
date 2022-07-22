390be2094ffc40828deff0f0d196c339cf16b7aa
HeYuJie <1445499792@qq.com>
2022-07-22 14:12:37 +0800

    脚本

--- deploy.bat
+++ deploy.bat
@@ -1,1 +1,5 @@
+@REM npm config set https-proxy http://127.0.0.1:10809
+@REM npm config set proxy http://127.0.0.1:10809
+@REM npm config set https-proxy null
+@REM npm config set proxy null
 npm publish
